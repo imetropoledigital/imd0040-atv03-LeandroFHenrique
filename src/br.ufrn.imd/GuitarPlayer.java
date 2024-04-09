b7951386fad46e9fb09eda54e2acb8888b00b8f4
LeandroFerreira <57442704+LeandroFHenrique@users.noreply.github.com>
2024-04-09 23:09:54 +0000

    .

--- src/br.ufrn.imd/GuitarPlayer.java
+++ src/br.ufrn.imd/GuitarPlayer.java
@@ -1,9 +1,9 @@
 package br.ufrn.imd;
 
 public class GuitarPlayer extends Musician {
-    private int howManyYears;
-    private boolean playElectronicGuitar;
-    private boolean playAcousticGuitar;
+    public int howManyYears;
+    public boolean playElectronicGuitar;
+    public boolean playAcousticGuitar;
 
     public GuitarPlayer(String name, String hometown, boolean playClassicalMusic, boolean canImprovise, boolean canCompose, int howManyYears, boolean playElectronicGuitar, boolean playAcousticGuitar) {
         super(name, hometown, playClassicalMusic, canImprovise, canCompose);
--- src/br.ufrn.imd/Musician.java
+++ src/br.ufrn.imd/Musician.java
@@ -1,9 +1,9 @@
 package br.ufrn.imd;
 
 public class Musician extends Person {
-    private boolean playClassicalMusic;
-    private boolean canImprovise;
-    private boolean canCompose;
+    public boolean playClassicalMusic;
+    public boolean canImprovise;
+    public boolean canCompose;
 
     public Musician(String name, String hometown, boolean playClassicalMusic, boolean canImprovise, boolean canCompose) {
         super(name, hometown);
